ff063fd84794db69ec9d6a0c515978c889236d65
DirtyDian <130277415+DirtyDian@users.noreply.github.com>
2023-05-04 15:40:05 +0600

    Initial commit

---
+++ CheseeFactory/Domain/Data/Employee.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.DataAnnotations;
+
+namespace CheseeFactory.Domain.Data
+{
+    class Employee
+    {
+        
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
